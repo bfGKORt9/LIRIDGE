64b4fb366e78feca0aa3f0695d23f90015d9c82b
GBY <92464683+bfGKORt9@users.noreply.github.com>
2026-07-06 09:09:55 +0000

    Complete native setup and dependencies

---
+++ target/debug/build/liridge_native-09734210e45a01d7/build_script_build-09734210e45a01d7.d
@@ -0,0 +1,5 @@
+/workspaces/LIRIDGE/target/debug/build/liridge_native-09734210e45a01d7/build_script_build-09734210e45a01d7.d: build.rs
+
+/workspaces/LIRIDGE/target/debug/build/liridge_native-09734210e45a01d7/build_script_build-09734210e45a01d7: build.rs
+
+build.rs:
